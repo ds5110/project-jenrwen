6bc423941df88bc5071c390193ec359920ac3c72
jrwen <35855687+jrwen@users.noreply.github.com>
2022-08-03 20:18:55 -0400

    used to produce the organized data without the unknown colum in the middle

--- acap_me/organize2018.py
+++ acap_me/organize2018.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-filename = "Family_outcomes_2021-2022.csv"
+filename = "Family_outcomes_2018-2019.csv"
 
 # The encoding option allows for non-standard characters (probably of Windows origin)
 df = pd.read_csv(filename, encoding = 'unicode_escape')
@@ -16,7 +16,7 @@ df.rename(columns=col, inplace=True)
 print(df.head(3))
 
 df.drop([0,1,2,3], axis=0, inplace=True)
-
+df = df[~df["Family"].str.contains("Gains", na=False)]
 # drop the empty columns
 df.dropna(how='all', axis=1, inplace=True)
 
@@ -30,13 +30,13 @@ df["Classroom"] = None
 df.ffill(axis=1, inplace=True)
 df.ffill(axis=0, limit=1, inplace=True)
 
-df = df[~df["Family"].str.contains('Average|complete|Scoring|Gains', regex=True)]
 
 #df.Classroom = df.Classroom.replace(to_replace="^[0-9]{2}$", value=None)
 df.Classroom = df.Classroom[~df.Classroom.str.contains('^[0-9]{2}$', regex=True)]
 df.ffill(axis=0, inplace=True)
 df = df[~df["Family"].str.contains("ACAP", na=False)]
 
+df = df[~df["Family"].str.contains('Average|complete|Scoring|Gains', regex=True)]
 #filter = isinstance(df["Classroom"], str)
 #df["Classroom"].where(filter,inplace=True)
 
@@ -51,7 +51,7 @@ df = df[~df["Family"].str.contains("ACAP", na=False)]
 #df["Classroom"] = df["Classroom"].replace(88,None)
 
 df.drop_duplicates(inplace=True)
-df["Year"] = "2021-2022"
+df["Year"] = "2018-2019"
 
 # This function replaces a name in the first column with with the integer from column 3
 #def clean(row):
@@ -69,6 +69,6 @@ df["Year"] = "2021-2022"
 #df[df[0].str.contains("ACAP")==False]
 
 # Write the cleaned data to a CSV file
-df.to_csv("2021-2022_assessment_organized.csv", index=False)
+df.to_csv("2018-2019_assessment_organized.csv", index=False)
 
 print("organized")

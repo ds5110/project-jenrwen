8909f1efa6255035d3596360febe2e6095ebf6b0
Evan-Chan-NEU <105451823+Evan-Chan-NEU@users.noreply.github.com>
2022-08-08 11:48:13 -0400

    Update Number_Families_by_Year.py

--- src/Number_Families_by_Year.py
+++ src/Number_Families_by_Year.py
@@ -12,8 +12,10 @@ def plot_num_fam(df):
   family_count_plot = sns.barplot(x = family_count.index, y = family_count.values, 
                                   order = ['2018-2019','2019-2020','2020-2021', '2021-2022'])
   sns.set(rc = {'figure.figsize':(15,10)})
+  sns.set_style("darkgrid")
   family_count_plot.set_xlabel("School Year", fontsize = 20)
   family_count_plot.set_ylabel("Number of Families Enrolled", fontsize = 20)
   family_count_plot.set_title("Number of Families Enrolled by Year", fontsize=20)
+  plt.show()
   
 plot_num_fam(df)
